85bea25deaba4df520639a362eba9672d7fc0802
rishank14 <rishankkalra@gmail.com>
2025-05-14 20:07:42 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,13 +17,13 @@ const registerUser = asyncHandler(async (req, res) => {
   // send response
 
   const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+  // console.log("email", email);
 
   if (!fullName || !email || !username || !password) {
     throw new ApiError(400, "Please fill all the fields");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
 
@@ -31,9 +31,17 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User already exists");
   }
 
+  // console.log(req.files);
+  
   const avatarLocalPath = req.files?.avatar[0]?.path;
 
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Please upload an avatar");
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file uploaded successfully
-    console.log("File uploaded successfully", response.url);
+    // console.log("File uploaded successfully", response.url);
+    fs.unlinkSync(localFilePath); //remove the file from local storage
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the file from local storage
